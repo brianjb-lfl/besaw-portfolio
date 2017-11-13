d0905ddc5ed22a610f4c7d29dbddb9e6c3c3bca3
Brian Besaw <bjbesaw@outlook.com>
2017-11-13 09:23:43 -0500

    added project summary data to content module

--- content.js
+++ content.js
@@ -7,4 +7,19 @@ const content = {
     '<p>This is my bio text.  Some words are <strong>highlighted</strong> for emphasis.  Here I talk about things I like; my dogs, motorcycle, living in a warm climate, etc.</p>' +
     '<p>In this paragraph I talk about tech stuff like <strong>node</strong> and <strong>react</strong>.  Maybe I mention how I\'ve always loved programming and learning new languages like <strong>python</strong>.</p>',
 
+  projectSummInfo:
+    [
+      {
+        id: 1,
+        img: "./img/buzz-kill-main.png",
+        text: 'Buzz-Kill helps establishments that serve alcohol provide for their patrons\' safety and guest experience by maintaining a real-time estimate of each patron\'s bac in a user-friendly format.'
+      }
+
+
+
+
+
+    ]
+
+
 };

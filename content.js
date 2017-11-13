95de0532a24fc49cbd595bb915ab0ca287ed8e4c
Brian Besaw <bjbesaw@outlook.com>
2017-11-13 09:00:38 -0500

    added content module to organize large blocks of text/code

---
+++ content.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const content = {
+
+  bioText: 
+    '<img id="bio-img" src="./img/besawb1708s.jpg">' +
+    '<p>This is my bio text.  Some words are <strong>highlighted</strong> for emphasis.  Here I talk about things I like; my dogs, motorcycle, living in a warm climate, etc.</p>' +
+    '<p>In this paragraph I talk about tech stuff like <strong>node</strong> and <strong>react</strong>.  Maybe I mention how I\'ve always loved programming and learning new languages like <strong>python</strong>.</p>',
+
+};
--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 'use strict';
-/*global STORE $ */
+/*global STORE content $ */
 
 
 function render() {
@@ -13,16 +13,22 @@ function render() {
   };
 
   $('.nav-link').removeClass('nav-selected');
+  
   if(STORE.appState !== 'front') {
     $(`li:contains("${STORE.appState}")`).addClass('nav-selected');
   }
 
   $('.panel').addClass('hidden');
+  
   if(!STORE.projDetail){
     $(`#${panelSelector[STORE.appState]}`).removeClass('hidden');
   }
   else {
     $(`#projdet-panel`).removeClass('hidden');
+  }
+
+  if(STORE.appState === 'bio'){
+    $('#bio-body').html(content.bioText);
   }
 
   if (STORE.navItems === 'hidden') {
@@ -34,6 +40,9 @@ function render() {
     $('#nav-items').removeClass('hidden');   
   }
 
+  // <img id="bio-img" src="./img/besawb1708s.jpg">
+  // <p>This is my bio text.  Some words are <strong>highlighted</strong> for emphasis.  Here I talk about things I like; my dogs, motorcycle, living in a warm climate, etc.</p>
+  // <p>In this paragraph I talk about tech stuff like <strong>node</strong> and <strong>react</strong>.  Maybe I mention how I've always loved programming and learning new languages like <strong>python</strong>.</p>
 
 }
 
